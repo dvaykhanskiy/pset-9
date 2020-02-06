d484912679d5b003b0ca8e200f98d2c6f1de6e9b
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-02-05 23:31:09 -0500

    inserted code from pset-8

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,66 +3,68 @@ import java.util.ArrayList;
 public class Exercises {
 
 	public boolean commonEnd(ArrayList<Integer> a, ArrayList<Integer> b) {
-		if (a == null || a.isEmpty() || b == null || b.isEmpty()) {
+		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
+		} else {
+			if ((a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1])) {
+				return true;
+			} else {
+				return false;
+			}
 		}
-		
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
 	}
-	
+
 	public ArrayList<String> endsMeet(ArrayList<String> values, int n) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
-	
+
 	public int difference(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public double biggest(ArrayList<Double> numbers) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
-	
+
 	public ArrayList<String> middle(ArrayList<String> values) {
 		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(ArrayList<Integer> numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(ArrayList<String> values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
